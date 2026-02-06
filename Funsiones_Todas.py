307a1a3d8885102265eb284f58dca858bb5fa55b
Ronald Ponce <geosist5@gmail.com>
2026-02-06 16:02:56 -0500

    Esta es la primera version creada por el Profe

---
+++ Funsiones_Todas.py
@@ -0,0 +1,6 @@
+# Evaluación Parcial 01 - Profesor Ronald Ponce
+# Programa establecido para entrega de funsiones de los 7 Grupos
+print("Sean toos bienvenodos estimadoa estudiantes")
+print ("Muy buena suerte y exitos en su parcial_01")
+
+# De aqui en adelante integre sus trabajos del parcial
