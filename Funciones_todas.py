cb24e3501e1a328d37c36cfbe0690febedbb3265
John Miró <john.miiiro@gmail.com>
2026-02-09 10:56:24 -0500

    Cambio modificado.

--- Funciones_todas.py
+++ Funciones_todas.py
@@ -38,3 +38,39 @@ def obtener_positivos(lista):
 mi_lista = [10, -5, 8, -1, -22, 15]
 print(f"Isacar Hernandez: Negativos son {obtener_negativos(mi_lista)}")
 print(f"Eimy: Positivos son {obtener_positivos(mi_lista)}")
+
+
+
+
+#GRUPO #3
+# Cadena - Número - Cadena vacía
+#*********************************
+#** Desarrollado por: Freinanyelis Gomez        ID: 159935684 **
+#*****************************************
+def contar_caracteres(cadena):
+   return len(cadena)
+
+# *********** PRUEBA DE FUNCIONAMIENTO ***********
+texto = input("Ingrese una cadena de texto: ")
+resultado = contar_caracteres(texto)
+print("freinanyelis gomez - Resultado de la función:", resultado)
+
+
+#Cadena-Vacía
+#*********************************
+#** Desarrollado por: John Miró  ID: 10-715-2197 **
+#******************************************************
+def cadena_vacia(cadena):
+    if cadena == "":
+        return True
+    else:
+        return False
+
+
+# Ejemplo de uso
+texto = input("Ingrese una cadena de texto: ")
+
+if cadena_vacia(texto):
+    print("La cadena está vacía")
+else:
+    print("La cadena NO está vacía")
