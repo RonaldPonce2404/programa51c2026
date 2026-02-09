da988d3880f29e763f0b9021c7ae713c8b5ae091
Isacar Hernandez <isacarhernandez40@gmail.com>
2026-02-09 09:58:35 -0500

    Actualizando: Integración Grupo 6 - Código completo

--- FunsionG6.py
+++ FunsionG6.py
@@ -1,1 +1,12 @@
-def obtener_negativos(l): return [n for n in l if n < 0]
+# Raíz cuadrada - Ecuación-lineal 
+# Desarrollado por: Isacar Hernandez y Eimy  
+
+def obtener_negativos(lista):
+    return [n for n in lista if n < 0]
+
+def obtener_positivos(lista):
+    return [n for n in lista if n > 0]
+
+mi_lista = [10, -5, 8, -1, -22, 15]
+print(f"Isacar Hernandez: Negativos son {obtener_negativos(mi_lista)}")
+print(f"Eimy: Positivos son {obtener_positivos(mi_lista)}")
