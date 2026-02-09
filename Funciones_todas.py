a97a7fa18f7a64cf71711648966d6eed4ce403b3
Isacar Hernandez <isacarhernandez40@gmail.com>
2026-02-09 10:57:24 -0500

    Grupo 6: Mi parte añadida al archivo del profesor

--- Funciones_todas.py
+++ Funciones_todas.py
@@ -34,7 +34,7 @@ def obtener_negativos(lista):
 
 def obtener_positivos(lista):
     return [n for n in lista if n > 0]
-
+#Isacar y Eimy
 mi_lista = [10, -5, 8, -1, -22, 15]
 print(f"Isacar Hernandez: Negativos son {obtener_negativos(mi_lista)}")
 print(f"Eimy: Positivos son {obtener_positivos(mi_lista)}")
